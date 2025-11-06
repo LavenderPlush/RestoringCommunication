0a5905b569792375e0ceb8b2d435d7c2c05e4caa
Olivier-Baptiste Hansen <oliha@itu.dk>
2025-11-06 18:04:50 +0100

    Add all human sounds
    Co-authored-by: Johan Brandi <johbr@itu.dk>

--- Entities/Players/Movement/movement.gd
+++ Entities/Players/Movement/movement.gd
@@ -13,9 +13,20 @@ var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 @export var control_prefix: String
 @export var body: CharacterBody3D
 
+@export_group("Sound")
+@export var footstep_sound_timer: Timer
+@export var jump_emitter: FmodEventEmitter3D
+@export var footstep_emitter: FmodEventEmitter3D
+@export var land_emitter: FmodEventEmitter3D
+
 # Exposed
 var is_climbing: bool = false
 var is_facing_left: bool = false
+var is_walking: bool = false
+var is_falling: bool = false
+
+func _ready() -> void:
+	footstep_sound_timer.timeout.connect(_on_footstep_timer)
 
 func process_climb(can_climb: bool):
 	var climb_input_vertical = Input.get_axis(
@@ -40,11 +51,14 @@ func process_gravity(delta: float):
 		body.velocity.y -= gravity * delta
 
 func process_jump():
-	if is_climbing:
-		body.velocity.y = 0
-		is_climbing = false
+	if body.is_on_floor() and is_falling:
+		is_falling = false
+		_play_sound(land_emitter)
+	elif not body.is_on_floor():
+		is_falling = true
 	if (Input.is_action_just_pressed("%s_jump" % control_prefix)
 		and body.is_on_floor()):
+			_play_sound(jump_emitter)
 			body.velocity.y = jump_velocity
 
 func process_movement():
@@ -59,10 +73,30 @@ func process_movement():
 	direction.z = input_direction.x
 
 	if direction:
+		_handle_walk_sound()
 		body.velocity.z = direction.z * speed
 		_set_facing_direction(direction)
 	else:
+		is_walking = false
 		body.velocity.z = move_toward(body.velocity.z, 0, speed)
+
+func _handle_walk_sound():
+	if not is_walking and body.is_on_floor():
+		_play_sound(footstep_emitter)
+		is_walking = true
+		footstep_sound_timer.start()
+
+func _play_sound(emitter: FmodEventEmitter3D):
+	if emitter:
+		emitter.play()
 
 func _set_facing_direction(direction: Vector3) -> void:
 	is_facing_left = true if direction.z < 0 else false
+
+# Signals
+func _on_footstep_timer():
+	if not is_walking or not body.is_on_floor():
+		return
+	_play_sound(footstep_emitter)
+	footstep_sound_timer.start()
+	
