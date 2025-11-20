6b7c1d4371cba1910fa201908e175fb979da5469
Olivier-Baptiste Hansen <oliha@itu.dk>
2025-11-20 23:10:01 +0100

    Add test scene and skipping

--- CutscenePlayer/cutscene_base.gd
+++ CutscenePlayer/cutscene_base.gd
@@ -5,25 +5,24 @@ class_name CutscenePlayer extends GameScene
 
 @export_category("Developers")
 @export var frame_holder: Control
+@export var wait_timer: Timer
 
 var frames: Array[Frame] = []
 var current_frame: int = 0
 
 func _ready() -> void:
 	load_frames()
-	
 	if frames.size() == 0:
 		finish_scene()
 		return
 	
 	# Wait before playing
-	await wait()
+	wait_timer.timeout.connect(play_frame)
+	wait_timer.start(wait_time)
 
-	# Play first frame
-	play_frame()
-
-func wait() -> void:
-	await get_tree().create_timer(wait_time).timeout
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("alien_jump") or event.is_action_pressed("human_jump"):
+			force_skip()
 
 func load_frames() -> void:
 	var raw_frames = frame_holder.get_children()
@@ -35,18 +34,19 @@ func load_frames() -> void:
 		frames[frame].finish.connect(frame_finish)
 
 func play_frame() -> void:
-	frames[current_frame].play(current_frame)
+	if current_frame > frames.size() - 1:
+		finish_scene()
+	else:
+		frames[current_frame].play(current_frame)
+		current_frame += 1
 
-func skip_frame() -> void:
-	frames[current_frame].stop()
-	current_frame += 1
+func force_skip() -> void:
+	if wait_timer.time_left > 0:
+		wait_timer.stop()
+	frames[current_frame - 1].stop()
 	play_frame()
 
 # Signals
 func frame_finish(id) -> void:
-	if id == current_frame:
-		if current_frame == frames.size() - 1:
-			finish_scene()
-		else:
-			current_frame += 1
-			play_frame()
+	if id == current_frame - 1:
+		play_frame()
