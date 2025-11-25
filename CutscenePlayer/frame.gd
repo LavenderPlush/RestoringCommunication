014dcba89feda6f0cfb06a1e09676a8d55842aa6
SnowyPeax <johbr@itu.dk>
2025-11-25 20:37:28 +0100

    add adaptive background music to cutscenes

--- CutscenePlayer/frame.gd
+++ CutscenePlayer/frame.gd
@@ -6,6 +6,8 @@ class_name Frame extends TextureRect
 
 @export_group("Sound")
 @export var emitter: FmodEventEmitter3D
+@export var ambience: String = "Chill":
+	get(): return ambience
 
 var id: int
 
