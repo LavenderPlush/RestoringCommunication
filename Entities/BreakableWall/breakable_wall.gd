d9f3e9af47ec6f7a51ebae49f184ab01efd204b9
SnowyPeax <johbr@itu.dk>
2025-11-27 11:36:39 +0100

    add wall-break-sound
    Co-authored-by: Olivier <oliha@itu.dk>

--- Entities/BreakableWall/breakable_wall.gd
+++ Entities/BreakableWall/breakable_wall.gd
@@ -4,10 +4,13 @@ extends StaticBody3D
 ##This Object will keep resetting until Players reach this Checkpoint ID. Keep at -1 to always reset.
 @export var reset_until_checkpoint_id: int = -1
 
+
 @onready var destruction_area: Area3D = $DestructionArea
 @onready var destruction_collider: CollisionShape3D = $DestructionArea/CollisionShape3D
 @onready var mesh: MeshInstance3D = $Mesh/Cube_003
 @onready var collider: CollisionShape3D = $CollisionShape3D
+
+@onready var break_sound_emitter: FmodEventEmitter3D = $BreakSoundEmitter
 
 var wall_break_particles: PackedScene = preload("res://Entities/BreakableWall/wall_break_particles.tscn")
 
@@ -29,6 +32,8 @@ func _on_destruction_area_entered(body: Node3D) -> void:
 		get_parent().add_child(particles)
 
 		particles.global_position = global_position
+
+		break_sound_emitter.play()
 
 		_set_active(false)
 
