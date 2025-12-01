efcc12bc997192061068e994729e5efbc78716f2
Andrei Mataoanu <amatao1999@gmail.com>
2025-12-01 20:39:22 +0100

    Changed starting scene. Added pause to levels.

--- Menus/Pause/pause_menu.gd
+++ Menus/Pause/pause_menu.gd
@@ -29,7 +29,7 @@ func _on_resume_button_pressed() -> void:
 
 func _on_main_menu_button_pressed() -> void:
 	get_tree().paused = false
-	get_tree().change_scene_to_file("res://Entities/Menus/Main Menu/main_menu.tscn")
+	get_tree().change_scene_to_file("res://Menus/Main Menu/main_menu.tscn")
 
 func _on_quit_button_pressed() -> void:
 	get_tree().quit()
