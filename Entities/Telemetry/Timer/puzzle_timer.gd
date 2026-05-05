8f4bcedc68df392bd5115af15ac8984b7900ee5a
Olivier-Baptiste Hansen <oliha@itu.dk>
2026-05-05 11:00:08 +0200

    Add fixes

--- Entities/Telemetry/Timer/puzzle_timer.gd
+++ Entities/Telemetry/Timer/puzzle_timer.gd
@@ -10,22 +10,26 @@ extends Node
 
 var timing: bool = false
 var time: float = 0
+var completed: bool = false
 
 func _ready() -> void:
-	start.body_entered.connect(start_entered)
+	if start:
+		start.body_entered.connect(start_entered)
+	else:
+		timing = true
 	end.body_entered.connect(end_entered)
 
 func _process(delta: float) -> void:
-	if not timing:
+	if not timing or completed:
 		return
 	time += delta
 
 func start_entered(body: Node3D):
-	if body is Player and !timing:
+	if body is Player and !timing and !completed:
 		timing = true
 	
 func end_entered(body: Node3D):
-	if body is not Player or !timing:
+	if body is not Player or !timing or completed:
 		return
 	timing = false
 	var data: Dictionary[String, Variant] = {
@@ -33,4 +37,4 @@ func end_entered(body: Node3D):
 	}
 	Talo.events.track(player + " puzzle", data)
 	Talo.events.flush()
-	set_process(false)
+	completed = true
